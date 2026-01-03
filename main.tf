7f5ca949690ea0355977a42778c12a74c710a0e5
unknown <stalinbe15@gmail.com>
2026-01-03 15:01:26 +0530

    Updated File

--- main.tf
+++ main.tf
@@ -2,24 +2,40 @@ provider "aws" {
   region = "us-east-2"
 }
 
+# VPC
 resource "aws_vpc" "trend_vpc" {
   cidr_block = "10.0.0.0/16"
+
   tags = {
     Name = "trend-vpc"
   }
 }
 
-resource "aws_subnet" "public_subnet" {
+# Public Subnet 1
+resource "aws_subnet" "public_subnet_1" {
   vpc_id                  = aws_vpc.trend_vpc.id
   cidr_block              = "10.0.1.0/24"
-  map_public_ip_on_launch = true
   availability_zone       = "us-east-2a"
+  map_public_ip_on_launch = true
 
   tags = {
-    Name = "trend-public-subnet"
+    Name = "trend-public-subnet-1"
   }
 }
 
+# Public Subnet 2
+resource "aws_subnet" "public_subnet_2" {
+  vpc_id                  = aws_vpc.trend_vpc.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = "us-east-2b"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "trend-public-subnet-2"
+  }
+}
+
+# Internet Gateway
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.trend_vpc.id
 
@@ -28,6 +44,7 @@ resource "aws_internet_gateway" "igw" {
   }
 }
 
+# Route Table
 resource "aws_route_table" "public_rt" {
   vpc_id = aws_vpc.trend_vpc.id
 
@@ -37,11 +54,13 @@ resource "aws_route_table" "public_rt" {
   }
 }
 
-resource "aws_route_table_association" "public_assoc" {
-  subnet_id      = aws_subnet.public_subnet.id
+# Route Table Associations
+resource "aws_route_table_association" "public_assoc_1" {
+  subnet_id      = aws_subnet.public_subnet_1.id
   route_table_id = aws_route_table.public_rt.id
 }
 
-
-
-
+resource "aws_route_table_association" "public_assoc_2" {
+  subnet_id      = aws_subnet.public_subnet_2.id
+  route_table_id = aws_route_table.public_rt.id
+}
